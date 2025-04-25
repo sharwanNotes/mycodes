d99654547fd7e27bdfc9aad6bf1e88da61371a22
Sharwan kumar <try2sharwan@gmail.com>
2025-04-25 17:49:02 +0530

    myapp

---
+++ src/main/java/com/hcl/SecondAppApplication.java
@@ -0,0 +1,20 @@
+package com.hcl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class SecondAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondAppApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String hello() {
+		return "Hello World!";
+	}
+}
